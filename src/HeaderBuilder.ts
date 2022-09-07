ef9224582ef6eb95699b5802252ac24a9543382d
NNesh <mullruslan@yandex.ru>
2022-09-07 19:36:17 +0500

    feat(): implemented hierarchy checking - implemented checking of passed hierarchy - added deep copying of passed hierarchy - changed version to 0.1.0

--- src/HeaderBuilder.ts
+++ src/HeaderBuilder.ts
@@ -1,22 +1,66 @@
-import { Builder, BuilderContext, IsRoot, Root } from "./types";
+import { cloneDeep } from "lodash";
+import { Builder, BuilderContext, IsJoint, IsRoot, Root, Node, IsEndSite, Joint } from "./types";
 
 
 export interface HeaderBuilder extends Builder<string> {
     setHierarchy(root: Root): void;
 }
 
+
+function checkNode(node: Node, parent: Joint | null) {
+    if (IsJoint(node) || IsRoot(node)) {
+        if (node.children.length === 0) {
+            throw new Error("Joint must have 1 or more children");
+        }
+
+        if (!node.name) {
+            throw new Error("Joint must have a name");
+        }
+
+        if (node.channels.length !== node.offset.length) {
+            throw new Error("Channels and offset values are mismatched");
+        }
+
+        node.children.forEach((child) => checkNode(child, node));
+    } else if (IsEndSite(node)) {
+        if (!parent) {
+            throw new Error("End site node must have a parent");
+        }
+
+        if (parent.offset.length !== node.offset.length) {
+            throw new Error("End site should have the same channels as its parent");
+        }
+
+        if (parent.children.length > 1) {
+            throw new Error("A parent of the end site node cannot have more childrens than ");
+        }
+    } else {
+        throw new Error("Unacceptable node!");
+    }
+}
+
+function checkHierarchy(root: Root) {
+    if (!IsRoot(root)) {
+        throw new Error("Top node should be a root");
+    }
+
+    checkNode(root, null);
+}
+
 export default function getHeaderBuilder(context: BuilderContext): HeaderBuilder {
     return {
         setHierarchy(root: Root): void {
-            if (!IsRoot(root)) {
-                throw new Error("Top node should be a root");
-            }
-
             if (context.motion) {
                 throw new Error("Changing the hierarchy is not allowed after motion creating");
             }
 
-            context.root = root;
+            if (!root) {
+                throw new Error("null is not allowed");
+            }
+
+            checkHierarchy(root);
+
+            context.root = cloneDeep(root);
         },
         build(): string {
             return "";
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -14,6 +14,13 @@ describe("Builder", () => {
         it("can return the motion builder without root", () => {
             assert.throws(builder.motion);
         });
+
+        it("null is not allowed", () => {
+            assert.throws(() => {
+                // @ts-ignore
+                builder.header().setHierarchy(null);
+            });
+        });
     
         it("cannot accept a different type of the node on top of the hierarchy", () => {
             assert.throws(() => {
@@ -30,6 +37,10 @@ describe("Builder", () => {
     })
 
     describe("Motion initializing", () => {
+        before(() => {
+            builder = getBVHBuilder();
+        })
+
         it("cannot accept to add a frame without motion initializing", () => {
             assert.throws(() => builder.motion().addFrame({
                 values: [0, 1],
@@ -43,5 +54,149 @@ describe("Builder", () => {
         it("initializing without a hierarchy is not allowed", () => {
             assert.throws(() => builder.motion().init(0.1));
         })
+    });
+
+    describe("Hierarchy checking", () => {
+        before(() => {
+            builder = getBVHBuilder();
+        });
+
+        it("offset and channels matching is acceptable", () => {
+            assert.doesNotThrow(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "Root",
+                    offset: [1, 2],
+                    channels: ["Xrotation", "Xrotation"],
+                    children: [
+                        {
+                            type: "end-site",
+                            name: "",
+                            offset: [1, 2],
+                            channels: [],
+                        }
+                    ],
+                });
+            });
+        });
+
+        it("0 children is not allowed for joints", () => {
+            assert.throws(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "Root",
+                    offset: [1, 2],
+                    channels: ["Xrotation", "Yrotation"],
+                    children: [],
+                });
+            });
+        });
+
+        it("offset and channels mismatching is not allowed", () => {
+            assert.throws(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "Root",
+                    offset: [1, 2],
+                    channels: ["Xposition"],
+                    children: [
+                        {
+                            type: "end-site",
+                            name: "",
+                            offset: [1, 2],
+                            channels: [],
+                        }
+                    ],
+                });
+            });
+        });
+
+        it("empty name is not allowed for joints", () => {
+            assert.throws(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "",
+                    offset: [1, 2],
+                    channels: ["Xrotation", "Xrotation"],
+                    children: [
+                        {
+                            type: "end-site",
+                            name: "",
+                            offset: [1, 2],
+                            channels: [],
+                        }
+                    ],
+                });
+            });
+        });
+
+        it("root-joint-endsite is acceptable", () => {
+            assert.doesNotThrow(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "Root",
+                    offset: [1, 2],
+                    channels: ["Xrotation", "Xrotation"],
+                    children: [
+                        {
+                            type: "joint",
+                            name: "Leg",
+                            offset: [1, 2],
+                            channels: ["Xrotation", "Xrotation"],
+                            children: [
+                                {
+                                    type: "end-site",
+                                    name: "",
+                                    offset: [1, 2],
+                                    channels: [],
+                                }
+                            ],
+                        },
+                    ],
+                });
+            });
+        });
+
+        it("haveing end-site and other joints together is not acceptable", () => {
+            assert.throws(() => {
+                builder.header().setHierarchy({
+                    type: "root",
+                    name: "Root",
+                    offset: [1, 2],
+                    channels: ["Xrotation", "Xrotation"],
+                    children: [
+                        {
+                            type: "joint",
+                            name: "Hip",
+                            offset: [1, 2],
+                            channels: ["Xrotation", "Xrotation"],
+                            children: [
+                                {
+                                    type: "end-site",
+                                    name: "",
+                                    offset: [1, 2],
+                                    channels: [],
+                                },
+                                {
+                                    type: "joint",
+                                    name: "Knee",
+                                    offset: [5, 5],
+                                    channels: ["Xrotation", "Xrotation"],
+                                    children: [
+                                        {
+                                            type: "end-site",
+                                            name: "",
+                                            offset: [0.5, 0.5],
+                                            channels: [],
+                                        }
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                });
+            });
+        });
+
     });
 });
